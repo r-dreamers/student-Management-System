503711d429c762a693200f4cc9ee35acc73b12fc
momo <momo@momos-MacBook-Air.local>
2026-09-11 16:49:44 +0600

    added classes

--- p.py
+++ p.py
@@ -1,1 +1,7 @@
-print("momo")
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def show_info(self):
+        print(f"Name: {self.name} \nAge : {self.age}") 
